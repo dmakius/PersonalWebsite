24a8ef7cb927dd7c9978b63067c8289ff1356a74
Daniel Makover <dmakover@gmail.com>
2016-05-28 11:08:08 +0300

    pong game fix

--- public/Games/pongGame.js
+++ public/Games/pongGame.js
@@ -1,6 +1,6 @@
 var ready = function() {
 var HEIGHT = 500, WIDTH = 700, keys = [], canvas, ctx, score = 0, computerScore= 0, intro = true;
-
+var gameStart  = false;
 canvas = document.getElementById("myPongCanvas");
 ctx = canvas.getContext("2d");
 canvas.width = WIDTH;
@@ -29,26 +29,38 @@ var computer = {
 }
 
 function _init_pong(){
-		introFrame();
+
+
 }
 
 function mainLoop(){
-	updateFrame();
-	renderFrame();
+	if(gameStart == false){
+		introFrame();
+		if(keys[32]){gameStart = true}
+	}
+	else if( gameStart == true){
+		updateFrame();
+		renderFrame();
+	}
+	
 	window.requestAnimationFrame(mainLoop);
 }
-//function introFrame(){
-//	ctx.fillStyle = "black";
-//	ctx.fillRect(0,0, canvas.width, canvas.height);
-//	ctx.fillStyle = "red";
-//	ctx.font="bold 46px Arial";
-//	ctx.fillText("PONG: 2.0.1  The GAME", canvas.width/2 - 250, canvas.height/2);
-//	ctx.font="bold 24px Arial";
-//	ctx.fillText("The First to 11 WINS!!!!", 20, 325);
-//}
+
+function introFrame(){
+	ctx.fillStyle = "black";
+	ctx.fillRect(0,0, canvas.width, canvas.height);
+	ctx.fillStyle = "red";
+	ctx.font="bold 46px Arial";
+	ctx.fillText("PONG: 2.0.1  The GAME", canvas.width/2 - 250, canvas.height/2);
+	ctx.font="bold 24px Arial";
+	ctx.fillText("The First to 11 WINS!!!!", 20, 325);
+}
 
 function updateFrame()
 {
+	 var paddleMiddle = player.y + player.height/2;
+	 var computerMiddle = computer.y + computer.height/2;
+	
 	//the player's movements
 	if(keys[40]){player.y += 10;}//going down
 	if(keys[38]){player.y -= 10;}//going up
@@ -57,19 +69,38 @@ function updateFrame()
 	//the balls movements
 	ball.y += ball.velY;
 	ball.x += ball.velX;
+
+	//bouncing the ball off the sides
 	if(ball.y + ball.height >= canvas.height || ball.y <= 0){ball.velY *= -1}
-	//if(ball.x + ball.width >= canvas.width || ball.x <= 0){ball.velX *= -1}
 	
-	if(ball.x <= -50){ score += 1; serve(1)}
-	if(ball.x >= 750){ computerScore += 1; serve(2)}
+	//scoreing a point
+	if(ball.x <= 0){computerScore += 1;serve(1);}
+	if(ball.x >= 700){ score += 1; serve(2)}
 	
-	if(ball.y >= player.y && ball.y <= player.y + player.height && ball.x <= player.x +player.width){ball.velX *= -1;}//refelct off player paddle
-	if(ball.y >= computer.y && ball.y <= computer.y + computer.height && ball.x >= computer.x){ball.velX *= -1; }//refelct off computer paddle
+	//hiting the player paddle
+	if(ball.y >= player.y && //paddle bottom Y
+	 ball.y <= player.y + player.height && //player top Y
+	 ball.x <= player.x +player.width && //player right X
+	 ball.x >= player.x){ //player left X
+	 	ball.velX *= -1;
+		var ballYDev = ball.y  - paddleMiddle;
+		ball.velY = ballYDev*0.35
+	 }
+	 //refelct off player paddle
+	if(ball.y >= computer.y &&  //computer top Y
+		ball.y <= computer.y + computer.height &&  //computer buttom Y
+		ball.x >= computer.x && //computer left X
+		ball.x <= computer.x + computer.width){ //computer right X
+			ball.velX *= -1;
+			var ballYDev = ball.y  - computerMiddle;
+			ball.velY = ballYDev*0.35
+		}//refelct off computer paddle
 	
-	var desty = ball.y - (computer.height - ball.height)*0.5;
-	//ease the movement toawrds the ideal poosition
-	computer.y += (desty - computer.y)*0.15;
-	
+	if(ball.x >= canvas.width/3){
+		var desty = ball.y - (computer.height - ball.height)*0.5;
+		//ease the movement toawrds the ideal poosition
+		computer.y += (desty - computer.y)*0.15;
+	}
 	computer.y = Math.min( Math.max(computer.y, 0), 400);
 	
 }
@@ -96,29 +127,30 @@ function renderFrame()
 	ctx.fillStyle = "red";
 	ctx.font="bold 24px Arial";
 	ctx.fillText("Player Score: " + score, 10, 475);
-	ctx.fillText("Computer Score: " + score, 475, 475);
-	drawNet()
+	ctx.fillText("Computer Score: " + computerScore, 475, 475);
+	drawNet();
 }
 
 function drawNet(){
 	var brickHeight = canvas.height / 10;
-	for(var i = 0; i < 10; i++)
-	{
+	for(var i = 0; i < 10; i++){
 		ctx.fillStyle = "green";
 		ctx.fillRect(canvas.width/2, brickHeight*i, 10, 20)
 	}
 }
-
-function serve(dir)
-{
+function serve(dir){
 	if(dir == 1)
 	{
-		ball.x = 450;
+		ball.x = canvas.width/2;
+		ball.y = canvas.height/2;
 		ball.velX = -5;
+		ball.velY = -5;
 	}
 	else if(dir == 2){
-		ball.x = 100;
+		ball.x = canvas.width/2;
+		ball.y = canvas.height/2;
 		ball.velX = 5;
+		ball.velY = 5;
 	}
 }
 	
@@ -133,8 +165,8 @@ document.body.addEventListener("keydown", function(e){
 
 document.body.addEventListener("keyup", function(e){
 	keys[e.keyCode] = false});
-
-mainLoop();	
+	
+	mainLoop();	
 }
+
 $(document).ready(ready);
-$(document).on('page:load', ready);
