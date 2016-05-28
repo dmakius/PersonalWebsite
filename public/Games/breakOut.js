157bff3932f82272ee6936651d1cfabba159f248
Daniel Makover <dmakover@gmail.com>
2016-05-28 13:21:09 +0300

    breakout fix

--- public/Games/breakOut.js
+++ public/Games/breakOut.js
@@ -5,7 +5,7 @@ canvas = document.getElementById("myBreakoutCanvas");
 ctx = canvas.getContext("2d");
 canvas.width = WIDTH;
 canvas.height = HEIGHT;
-paddle = false;
+var paddle = false;
 
 var keys = [];
 
@@ -16,11 +16,12 @@ var brickHeight = 25;
 var brickWidth = 70;
 
 var level1 = [
+	[0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0],
+	[2,1,2,1,2,1,2,1,2,1],
 	[1,2,1,2,1,2,1,2,1,2],
 	[2,1,2,1,2,1,2,1,2,1],
-	[1,1,1,2,1,2,1,2,1,2],
-	[2,1,1,1,2,1,2,1,2,1],
-	[1,2,1,1,1,2,1,2,1,2],
+	[1,2,1,2,1,2,1,2,1,2],
 	[2,1,2,1,2,1,2,1,2,1],
 	[1,2,1,2,1,2,1,2,1,2],
 ]
@@ -60,176 +61,108 @@ function populateBricks()
 }
 
 
-function checkCollisions(object1, object2, isPaddle)
-{
-
+function checkCollisions(object1, object2){
 	var vectorX = (object1.x + object1.width/2) - (object2.x + object2.width/2);
-	var vectorY = (object1.y + object1.height/2) - (object2.y + object2.height/2);
-	
+	var vectorY = (object1.y + object1.height/2) - (object2.y + object2.height/2);	
 	var hHeight = (object1.height/2) + (object2.height/2);
 	var hWidth = (object1.width/2) + (object2.width/2);
 	
 	//check for a collision
-	if((Math.abs(vectorX) <= hWidth) && (Math.abs(vectorY) <= hHeight) && (object2.type >= 1 || object2.fill == "hard"))
-	{
-		
+	if((Math.abs(vectorX) <= hWidth) && (Math.abs(vectorY) <= hHeight) &&
+	 (object2.type >= 1 || object2.fill == "hard")){	
 		var oX = hWidth - Math.abs(vectorX);//left over on x coordinate
 		var oY = hHeight - Math.abs(vectorY);//left over on y coodinate
 		
 		object2.type = object2.type - 1;
-		if(!object2.fill)
-		{
-			score += 10;
-		}
 		
-		
-		if(object2.type <= 0)
-		{
-			object2.type = 0;
-		}
+		if(!object2.fill){score += 10;}
+		if(object2.type <= 0){object2.type = 0;}
 		
 		collision  = null;
 		
 		if(oX >= oY) //up or down collision
 		{
-			if(vectorY > 0)
-			{
-				collision = "t";
-			}
-			else
-			{
-				collision = "b";
-			}
+			if(vectorY > 0){collision = "t";}
+			else{collision = "b";}
 		}
-		//detect down collision with paddle
-		if((oX >= oY) && (vectorY < 0) && (isPaddle = true)){ collision = "bb"; paddle = false;}
-
 		else //left or right collision
 		{
-			if(vectorX > 0)
-			{
-				collision = "l";
-			}
-			else
-			{
-				collision = "r";
-			}
+			if(vectorX > 0){collision = "l";}
+			else{collision = "r";}
 		}
-		
 	return collision
 	}
 }
 
-function translateCollision(collision)
-{
+function translateCollision(collision){
 	if(collision == "t"){ball.velY *= -1;}
 	if(collision == "b"){ball.velY *= -1;}
 	if(collision == "l"){ball.velX *= -1;}
 	if(collision == "r"){ball.velX *= -1;}
-	if(collision == "bb"){alert();}
-
 }
-
-function drawLevel()
-{
-	for (var i = 0; i < BRICKS.length; i++)
-	{
+function drawLevel(){
+	for (var i = 0; i < BRICKS.length; i++){
 		if(BRICKS[i].type == 2){ctx.fillStyle = "green";}
 		if(BRICKS[i].type == 1){ctx.fillStyle = "purple";}
 		if(BRICKS[i].type == 0){ctx.fillStyle = "black";}
-		
 		ctx.fillRect(BRICKS[i].x, BRICKS[i].y, brickWidth, brickHeight);
 		ctx.strokeRect(BRICKS[i].x+ 1, BRICKS[i].y + 1, brickWidth - 2, brickHeight - 2);
 	}
 }
 
-function _init_breakout()
-{
+function _init_breakout(){
 	populateBricks();
 	mainLoop();
 }
 
-function mainLoop()
-{
-	if(!over)
-	{
-		updateFrame();
-	}
+function mainLoop(){
+	if(!over){updateFrame();}
 	renderFrame();
 	window.requestAnimationFrame(mainLoop);
 }
-function updateFrame()
-{
-	if(keys[39])//move player left
-	{
-		player.x += 10;
-	}
-	if(keys[37])//move player right
-	{
-		player.x -= 10;
-	}
-
-	if(player.x <= 0)//keep player in bounds on left
-	{
-		player.x = Math.max(player.x, 0);
-	}
-	
-	if((player.x + player.width) > 600)//keep player in bounds on right
-	{
-		player.x = Math.min(player.x, 600)
-	}
+function updateFrame(){
+	if(keys[39]){player.x += 10;}
+	if(keys[37]){player.x -= 10;}
+	if(player.x <= 0){player.x = Math.max(player.x, 0);}
+	if((player.x + player.width) > 600){player.x = Math.min(player.x, 600)}
 	
 	ball.x += ball.velX;
 	ball.y += ball.velY;	
 	
 	for(var i = 0; i < BRICKS.length; i++)//check brick collisions
 	{	
-		paddle = false;
 		collision = checkCollisions(ball, BRICKS[i], paddle);	
 		translateCollision(collision);	
 	}
 	
 	//check player and ball collisions
-	paddle = true;
-	collision = checkCollisions(ball, player, paddle);	
+	collision = checkCollisions(ball, player);	
 	translateCollision(collision);
-	
+	if(collision == 'b'){
+		var ballFrompPaddleCenter = ball.x - (player.x +player.width/2);
+		ball.velX = ballFrompPaddleCenter * 0.3;
+	}
+
 	//keep the ball inside
-	if(ball.x >= canvas.width || ball.x <= 0)
-	{
-		ball.velX *= -1;
-	}
-	if(ball.y <= 0)
-	{
-		ball.velY *= -1;
-	}
+	if(ball.x >= canvas.width || ball.x <= 0){ball.velX *= -1;}
+	if(ball.y <= 0){ball.velY *= -1;}
 	
 	//dying
-	if(ball.y >= canvas.height)
-	{
+	if(ball.y >= canvas.height +100){
 		lives -= 1;
-		if( lives == 0)
-		{
-			over =  true;
-		}
-		else
-		{
-			serve();
-		}
+		if(lives == 0){over =  true;}
+		else{serve();}
 	}
 }
 
-
-function serve()
-{
-	ball.x = canvas.width * Math.random();
-	ball.y = 350;
-	ball.velX *= -1;
-	ball.velY *= -1;
+function serve(){
+	ball.x = canvas.width/2;
+	ball.y = canvas.height/2;
+	ball.velX = 5;
+	ball.velY = 5;
 }
 
-function renderFrame()
-{
+function renderFrame(){
 	ctx.clearRect(0,0,canvas.width, canvas.height);
 	ctx.fillStyle = "black";
 	ctx.fillRect(0,0, canvas.width, canvas.height);
@@ -257,12 +190,12 @@ function renderFrame()
 	
 	//game over sign
 	if(over)
-	{
-	//	ctx.fillStyle = "orange";
-	//	ctx.font= "bold 64px Arial";
-	//	ctx.fillText("GAME OVER", 150, 300);
-	//var ending = document.getElementById("gameOver");
-	//ending.style.display = "block";
+	{	score = 0;
+		lives = 3;
+		BRICKS = [];
+		populateBricks();
+		serve();
+		over = false;
 	}	
 }
 
@@ -275,4 +208,3 @@ document.body.addEventListener("keyup", function(e){
 _init_breakout();
 }
 $(document).ready(ready);
-$(document).on('page:load', ready);
