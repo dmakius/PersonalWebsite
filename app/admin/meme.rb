342b2603a1f6598b472aafb20df8baa2fcc286f6
Daniel Makover <dmakover@gmail.com>
2016-06-23 01:21:01 +0300

    meme backend

--- app/admin/meme.rb
+++ app/admin/meme.rb
@@ -17,19 +17,30 @@ ActiveAdmin.register Meme do
 	      params.permit meme: [:avatar]
 	    end
 	end
-	
+
+	index do 
+		column :id
+		column "Images" do |meme|
+   			image_tag(meme.avatar(:thumb))
+  		end
+		column :created_at
+ 		actions
+	end
+	#for the Edit Page
 	form do |f|
 		f.inputs "Meme Details" do
 	    	f.input :avatar, :required => false, :as => :file
+
 	      # Will preview the image when the object is edited
 	    end
 	    f.actions
 	end
-
+	
+	##for the view page
 	show do |ad|
       attributes_table do
-        row :avatar do
-         
+        row "Picture" do |meme|
+   			image_tag(meme.avatar(:medium))
         end
         # Will display the image on show object page
       end
--- app/admin/picture.rb
+++ app/admin/picture.rb
@@ -6,6 +6,7 @@ ActiveAdmin.register Picture do
     end
   
 
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
