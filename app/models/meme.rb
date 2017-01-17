3e5ffc3c8c99792db9fda7d94650d4f8dc120275
Daniel Makover <dmakover@gmail.com>
2017-01-17 07:24:48 +0200

    meme CORS fix 2

--- app/models/meme.rb
+++ app/models/meme.rb
@@ -5,11 +5,11 @@ class Meme < ActiveRecord::Base
 	 else
 	#for some reasons this is causing errors
 	#need to investigate later
-		has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, 
-		default_url: "/images/:style/missing.png",
- 		:storage => :dropbox,
-    	:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-    	:path => ":style/:id_:filename"
+	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, 
+	default_url: "/images/:style/missing.png",
+ 	:storage => :dropbox,
+    :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+    :path => ":style/:id_:filename"
     end
     #validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
     validates_attachment :avatar, content_type: { content_type: ["image/jpg", "image/jpeg",     "image/png"] }
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
- 
+ # meme Routes
   get 'memes/all' => 'memes#all'
   resources :memes
   
