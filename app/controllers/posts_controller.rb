790e67dcc9733ee24ec40a0a28fcf5a30708bc03
Daniel Makover <dmakover@gmail.com>
2017-01-31 07:36:40 +0200

    posts controller JSON

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,10 @@
 class PostsController < ApplicationController
   def index
       @post = Post.all.order("created_at desc")
+      respond_to do |format|
+        format.html
+        format.json{ render json: @post}
+      end
   end
  
  def show
