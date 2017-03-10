c9c0d4ce0b649fe8bf6cef2a4d4ac63d08c5a6bd
Daniel Makover <dmakover@gmail.com>
2017-03-11 00:58:15 +0200

    weather app: images added

--- public/weather.js
+++ public/weather.js
@@ -33,6 +33,7 @@ weatherApp.config(function($routeProvider){
 //SERVICES
 weatherApp.service('mainService', function(){
 	this.city = "";
+	this.mapKey = "";
 });
 
 //CONTROLLERS
@@ -49,12 +50,18 @@ weatherApp.controller("forcastController",['$scope', '$resource','mainService',f
 	$scope.city = mainService.city;
 	$scope.cnt = 7;
 	$scope.forecast = [];
+	$scope.map = mainService.mapKey;
 
 	$scope.addMap =function(){
 		 var map = document.getElementById("google_map");
 		 console.log(map);
-		 map.src = "https://www.google.com/maps/embed/v1/place?key=AIzaSyCVyMA5TUXJAmmklhlvK8kYv7n8FhNRPjE&q=" + mainService.city;
-	
+		 if($scope.map == ""){
+		 	map.src = "https://www.google.com/maps/embed/v1/place?key=AIzaSyCVyMA5TUXJAmmklhlvK8kYv7n8FhNRPjE&q=" + mainService.city;
+		 	$scope.map = map.src;
+		 }else{
+		 	map.src = $scope.map;
+		 }
+		
 	}();
 	
 	$scope.weatherAppId = 'e1f50df08bce0b26b34b813be1c63de3';
@@ -65,6 +72,23 @@ weatherApp.controller("forcastController",['$scope', '$resource','mainService',f
 	$scope.$watch('weather', function(){
 		$scope.forecast = $scope.weather
 	});
+
+	$scope.getWeatherImage = function(imageCode){
+		if(imageCode >= 200 && imageCode <= 230){return "http://openweathermap.org/img/w/09d.png";}
+		else if(imageCode >= 300 && imageCode <= 321){return "http://openweathermap.org/img/w/09d.png";}
+		else if(imageCode >= 500 && imageCode <= 504){return "http://openweathermap.org/img/w/10d.png";}
+		else if(imageCode == 511 ){return "http://openweathermap.org/img/w/13d.png";}
+		else if(imageCode >= 520 && imageCode <= 531){return "http://openweathermap.org/img/w/09d.png";}
+		else if(imageCode >= 600 && imageCode <= 631){return "http://openweathermap.org/img/w/13d.png";}
+		else if(imageCode >= 700 && imageCode <= 781){return "http://openweathermap.org/img/w/50d.png";}
+			//TO DO: Implement Logic for day/night images
+		else if(imageCode == 800){return "http://openweathermap.org/img/w/01d.png";}
+		else if(imageCode == 801){return "http://openweathermap.org/img/w/02d.png";}
+		else if(imageCode == 802){return "http://openweathermap.org/img/w/03d.png";}
+		else if(imageCode == 803){return "http://openweathermap.org/img/w/04d.png";}
+		else if(imageCode == 804){return "http://openweathermap.org/img/w/04d.png";}
+
+	}
 
 	$scope.convertToFahrenheit = function(degK){
 		return Math.round((1.8* (degK -273)) + 32);
@@ -77,4 +101,5 @@ weatherApp.controller("forcastController",['$scope', '$resource','mainService',f
 	$scope.convertDate = function(ts){
 		return new Date(ts *1000)
 	}
+
 }]);
