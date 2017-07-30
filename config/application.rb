520e813e627d8b9569d12c84a34d600ab30c7229
Daniel Makover <dmakover@gmail.com>
2017-07-30 09:54:04 -0400

    ckeditor heroku fix 1

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,7 @@ module PersonalWebsite
     config.action_mailer.delivery_method = :sendmail
     config.action_mailer.perform_deliveries = true
     config.action_mailer.raise_delivery_errors = true
+
+    config.assets.precompile += %w( ckeditor/* )
   end
 end
