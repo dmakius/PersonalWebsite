dc318d17b213bc7d44d7a297c9100d6c79d4dfe6
Daniel Makover <dmakover@gmail.com>
2017-07-30 00:08:44 -0400

    ckeditor configured

--- Gemfile
+++ Gemfile
@@ -22,6 +22,7 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
+gem 'ckeditor'
 # gem 'will_paginate'
 
 gem "paperclip", "~> 5.0.0.beta1"
--- app/admin/post.rb
+++ app/admin/post.rb
@@ -6,13 +6,21 @@ ActiveAdmin.register Post do
       column :created_at
       actions
   end
-  
+
   controller do
       def permitted_params
         params.permit!
       end
     end
 
+  form do |f|
+    f.inputs do
+        f.input :title
+        f.input :category
+        f.input :body, :as => :ckeditor
+        f.submit :style => "margin-left:15px;"
+    end
+  end
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
