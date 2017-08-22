4a9fdadc28b8c2f29ae46746b33c21c7a5eb780e
Daniel Makover <dmakover@gmail.com>
2017-08-21 20:12:14 -0400

    mmRunner: explosion image changed

--- public/Games/mmRunner/src/js/prefabs/Explosion.js
+++ public/Games/mmRunner/src/js/prefabs/Explosion.js
@@ -3,11 +3,11 @@ var MMRunner = MMRunner || {}
 MMRunner.Explosion = function(game, x , y){
   Phaser.Sprite.call(this, game, x, y, 'explosion1');
   this.anchor.setTo(0.5);
-  this.scale.setTo(2, 1.5);
+  this.scale.setTo(2);
   this.game.physics.arcade.enable(this);
   this.outOfBoundsKill = true;
   this.enableBody = true;
-  this.animations.add('explosion', [1,2,3], 8, true);
+  this.animations.add('explosion', [0,1,2,3], 16, true);
   this.animations.play('explosion');
   this.exlosionTime = this.game.time.now + 1000;
 }
@@ -17,7 +17,7 @@ MMRunner.Explosion.prototype.constructor = MMRunner.Explosion;
 MMRunner.Explosion.prototype.killTime = 0;
 MMRunner.Explosion.prototype.update = function(){
     this.killTime ++;
-    if(this.killTime >= 15){
+    if(this.killTime >= 16){
       this.destroy();
     }
 }
