f37b51ee5c65a983de9e8afb385f21d5d8db5ed4
Daniel Makover <dmakover@gmail.com>
2017-10-30 18:24:24 -0400

    aws added to meme model

--- app/models/meme.rb
+++ app/models/meme.rb
@@ -1,17 +1,12 @@
 class Meme < ActiveRecord::Base
-	# if Rails.env.development?
-	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
-	#  else
-	#for some reasons this is causing errors
-	#need to investigate later
-		# has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, 
-		# default_url: "/images/:style/missing.png",
- 	
- 	# 	:storage => :dropbox,
-  #   	:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-  #   	:path => ":style/:id_:filename"
-    # validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
-    # validates_attachment :avatar, content_type: { content_type: ["image/jpg", "image/jpeg",     "image/png"] }
+	has_attached_file :avatar, styles: {
+    thumb: '100x100>',
+    square: '200x200#',
+    medium: '300x300>',
+    large: '600x600>'
+  }
+
+  # Validate the attached image is image/jpg, image/png, etc
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
 
